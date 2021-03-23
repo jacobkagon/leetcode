75bf649b3dd2be701b78c2dbca038f818455ac8b
JakesProjects1 <jacob@kagon.net>
2021-03-23 17:44:06 -0400

    done

--- length-of-last-word.js
+++ length-of-last-word.js
@@ -1,3 +1,18 @@
 const lengthOfLastWord = function (s) {
   return s.trim().split(" ").pop().length;
 };
+
+
+//alternate solution
+
+var lengthOfLastWord = function(s) {
+    
+    
+  let length = 0, max = 0;
+  for (let i = 0; i < s.length; i++)
+      if (s[i] !== ' ')
+          length++, max = length;
+      else
+          length = 0;
+  return max;
+};
